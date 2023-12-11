a4677545cdd00359981ab8fe1cff77fec3a34aed
Abdalazez-Sabry <abdalazezsabry00@gmail.com>
2023-12-11 13:49:45 +0200

    a working suffix tree and test cases

--- suffixTree.cpp
+++ suffixTree.cpp
@@ -49,16 +49,20 @@ public:
     int length;
     char* str;
 
-    SuffixTree(char *str)
+    SuffixTree(const char* str)
     {
-        this->str = str;
-        length = strlen(str);
-        buildTree();
-
-        printf("Nodes: %d\n", nodeCounter);
+        buildTree(str);
     }
 
-    void buildTree() {
+    SuffixTree() = default;
+
+    void buildTree(const char* str) {
+        this->str = new char[strlen(str) + 1];
+        strcpy(this->str, str);
+        this->str[strlen(str)] = '\0';
+
+        length = strlen(str);
+
         root = new Node(-1, 0);
         nodeCounter++;
         int length = strlen(str);
@@ -114,21 +118,82 @@ public:
         node->appendChild(newNode);
     }
 
-    // void Search(const char* suffix) {
-    //     search(root, suffix);
-    // }
+    void Search(const char* suffix) {
+        search(root, suffix);
+    }
 
-    // void search(Node* node, const char* suffix) {
-    //     Node* matchingNode = nullptr;
-    //     Node* current = node->children;
-    //     while (current != nullptr) {
-    //         if (current->suffix[0] == suffix[0]) {
-    //             matchingNode = current;
-    //             break;
-    //         }
-    //     }
-        
-    // }
+    void search(Node* node, const char* suffix) {
+        int i = 0;
+        int suffixLength = strlen(suffix);
+
+        for (i = 0; i < node->length && i < suffixLength; i++) {
+            if (str[node->startIndex + i] != suffix[i]) {
+                printf("No match found");
+                return;
+            }
+        }
+
+
+        if (i == suffixLength) {
+            printAllLeavesIndexes(node);
+            // printf("\n");
+            return;
+        }
+
+        Node* current = node->children; 
+        while (current != nullptr) {
+            if (str[current->startIndex] == suffix[i]) {
+                char* newSuffix = new char[suffixLength - i];
+                strncpy(newSuffix, suffix + i, suffixLength - i);
+                newSuffix[suffixLength - i] = '\0';
+
+                search(current, newSuffix);
+                return;
+            }
+            current = current->next;
+        }
+
+        printf("No match found");
+    }
+
+    void printLeaves(Node* node) {
+        if (node->suffixIndex != -1) { 
+            printf("%d ", node->suffixIndex);
+        }
+
+        if (node->children != nullptr) {
+            printLeaves(node->children);
+        }
+
+        if (node->next != nullptr) {
+            printLeaves(node->next);
+        }
+    }
+
+    void printAllLeavesIndexes(Node* node) {
+        if (node->suffixIndex != -1) { 
+            printf("%d ", node->suffixIndex);
+        }
+
+        if (node->children != nullptr) {
+            printLeaves(node->children);
+        }
+    }
+};
+
+class TestSuffixTree {
+    SuffixTree tree;
+
+public: 
+    TestSuffixTree(const char* str) {
+        tree.buildTree(str);
+    }
+
+    void test(char* suffix, char* expected) {
+        printf("Result: ");
+        tree.Search(suffix); 
+        printf(", Expected: %s\n", expected);
+    }
 };
 
 int main()
@@ -137,11 +202,29 @@ int main()
 
     //            0123456789012
     SuffixTree t("bananabanaba$");
-
     // t.Search("ana"); // Prints: 1 3 7
     // t.Search("naba"); // Prints: 4 8
     
     // Add test cases here.
+    printf("Test1 : bananabanaba$\n");
+    TestSuffixTree t1("bananabanaba$");
+    t1.test("ana", "7 3 1");
+    t1.test("naba", "8 4");
+    t1.test("bana", "6 0");
+    t1.test("ban", "6 0");
+
+    printf("\nTest2 : suffixtree$\n");
+    TestSuffixTree t2("suffixtree$");
+    t2.test("tree", "6");
+    t2.test("ix", "4");
+    t2.test("f", "3 2");
+
+    printf("\nTest3 : hamhamhammamahamahm$\n");
+    TestSuffixTree t3("hamhamhammamahamahm$");
+    t3.test("ham", "13 6 3 0");
+    t3.test("mam", "9");
+    t3.test("z", "No match found");
+    t3.test("hamj", "No match found");
 
     return 0;
 }
